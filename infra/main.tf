87fb86cd58fc4d70a2add145f7a5240ceabc1cd6
Gnhee Cho <rmsgml0915@naver.com>
2026-06-22 23:36:30 +0900

    여러가지 수정

--- infra/main.tf
+++ infra/main.tf
@@ -95,6 +95,20 @@ resource "aws_cloudfront_distribution" "buhwalch_distribution" {
     viewer_protocol_policy = "redirect-to-https"    
   }  
 
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
